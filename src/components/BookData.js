e32d3567cc108dfaff04d6ae50c1792f5b52f61b
antonynans <antonynans@gmail.com>
2021-02-16 18:22:54 +0100

    initial commit

---
+++ src/components/BookData.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import UserTable from './table/userTable'; 
+
+
+export default function BookData() {
+    return (
+        <div className="container">
+            <h1>CRUD App with Hooks</h1>
+            <div className='flex-row'>
+                <div className='flex-large'>
+                    <h2>Add user</h2>
+                </div>
+                <div className='flex-large'>
+                    <h2>View users</h2>
+                    <UserTable />
+                </div>
+            </div>
+
+        </div>
+       
+    )
+}
