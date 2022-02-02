860278ba97afb3f96de6867d5790be8dff6dde1f
antonynans <antonynans@gmail.com>
2022-02-02 11:06:23 +0100

    edited the public folder

--- src/components/BookData.js
+++ src/components/BookData.js
@@ -1,67 +1,63 @@
-import React, { useState } from 'react'
-import AddUserForm from './AddUserForm';
-import UserTable from './table/userTable'; 
-import EditUserForm from './EditUserForm';
-
+import React, { useState } from "react";
+import AddUserForm from "./AddUserForm";
+import UserTable from "./table/userTable";
+import EditUserForm from "./EditUserForm";
 
 export default function BookData() {
-    const [users, setUsers] = useState(
-        [
-            {id: 1, name: 'Tania', username: 'Dj' },
-            {id: 2, name: 'Ben', username: 'MrBen' },
-            {id: 3, name: 'John', username: 'Johnny' }
-        ]
-    )
+  const [users, setUsers] = useState([
+    { id: 1, name: "Tania", username: "Dj" },
+    { id: 2, name: "Ben", username: "MrBen" },
+    { id: 3, name: "John", username: "Johnny" },
+  ]);
 
-    const [editing, setEditing] = useState(false);
-    const initialFormState = {id: null, name: '', username: '' }
-    const [currentUser, setCurrentUser] = useState(initialFormState)
-    const editRow = (user) => {
-        setEditing(true)
-        setCurrentUser({ id: user.id, name: user.name, username: user.username})
-    }
-    const updateUser = (id, updateUser) => {setEditing(false)
+  const [editing, setEditing] = useState(false);
+  const initialFormState = { id: null, name: "", username: "" };
+  const [currentUser, setCurrentUser] = useState(initialFormState);
+  const editRow = (user) => {
+    setEditing(true);
+    setCurrentUser({ id: user.id, name: user.name, username: user.username });
+  };
+  const updateUser = (id, updateUser) => {
+    setEditing(false);
 
-    setUsers(users.map((user) => (user.id === id ? updateUser : user)))
-    }
+    setUsers(users.map((user) => (user.id === id ? updateUser : user)));
+  };
 
-    const addUser = (user) => {
-        user.id = users.length + 1
-        setUsers([...users, user])
-    }
+  const addUser = (user) => {
+    user.id = users.length + 1;
+    setUsers([...users, user]);
+  };
 
-    const deleteUser = (id) => {
-        setUsers(users.filter((user) => user.id !== id))
-    }
+  const deleteUser = (id) => {
+    setUsers(users.filter((user) => user.id !== id));
+  };
 
-   
-    return (
-        <div className="container">
-            <h1 style={{color: 'rgb(69, 141, 255)'}}>User app with react hooks</h1>
-            <div className='flex-row'>
-                <div className='flex-large'>
-                    {editing ? (
-                        <div>
-                            <h2 style={{color: 'green'}}>Edit User</h2>
-                            <EditUserForm
-                                setEditing={setEditing}
-                                currentUser={currentUser}
-                                updateUser={updateUser}
-                            />
-                        </div>
-                    ) : (
-                        <div>
-                        <h2>Add User</h2>
-                        <AddUserForm addUser={addUser} />
-                    </div>
-                    )}
-                </div>
-                <div className='flex-large'>
-                    <h2>View Users</h2>
-                    <UserTable users={users} editRow={editRow} deleteUser={deleteUser} />
-                </div>
+  return (
+    <div className="container">
+      <h1 style={{ color: "rgb(69, 141, 255)" }}>User app with react hooks</h1>
+      <div className="flex-row">
+        <div className="flex-large">
+          {editing ? (
+            <div>
+              <h2 style={{ color: "green" }}>Edit User</h2>
+              <EditUserForm
+                setEditing={setEditing}
+                currentUser={currentUser}
+                updateUser={updateUser}
+              />
             </div>
+          ) : (
+            <div>
+              <h2>Add User</h2>
+              <AddUserForm addUser={addUser} />
+            </div>
+          )}
         </div>
-       
-    )
+        <div className="flex-large">
+          <h2>View Users</h2>
+          <UserTable users={users} editRow={editRow} deleteUser={deleteUser} />
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/EditUserForm.js
+++ src/components/EditUserForm.js
@@ -1,35 +1,51 @@
-import React, { useEffect, useState } from 'react';
-
+import React, { useEffect, useState } from "react";
 
 export default function EditUserForm(props) {
-    const [user, setUser] = useState(props.currentUser);
+  const [user, setUser] = useState(props.currentUser);
 
-    const handleInputChanege = e => {
-        setUser({
-            ...user,
-            [e.target.name]: e.target.value
-        })
-    }
+  const handleInputChanege = (e) => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-    const handleSubmit = e => {
-        e.preventDefault()
-            props.updateUser(user.id, user)
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.updateUser(user.id, user);
+  };
 
-    useEffect(() => {
-        setUser(props.currentUser)
-    }, [props])
+  useEffect(() => {
+    setUser(props.currentUser);
+  }, [props]);
 
-    return (
-        <form className='form' onSubmit={handleSubmit}>
-            <label>Name</label>
-            <input className='form-input' type='text' name='name' value={user.name} onChange={handleInputChanege} />
-            <label>Username</label>
-            <input className='form-input' type='text' name='username' value={user.username} onChange={handleInputChanege} />
-            <div className='button-container'>
-                <button className='form-button success'>Update user</button>
-                <button className='form-button danger' onClick={() => props.setEditing(false)}>Cancel</button>
-            </div>
-        </form>
-    )
+  return (
+    <form className="form" onSubmit={handleSubmit}>
+      <label>Name</label>
+      <input
+        className="form-input"
+        type="text"
+        name="name"
+        value={user.name}
+        onChange={handleInputChanege}
+      />
+      <label>Username</label>
+      <input
+        className="form-input"
+        type="text"
+        name="username"
+        value={user.username}
+        onChange={handleInputChanege}
+      />
+      <div className="button-container">
+        <button className="form-button success">Update user</button>
+        <button
+          className="form-button danger"
+          onClick={() => props.setEditing(false)}
+        >
+          Cancel
+        </button>
+      </div>
+    </form>
+  );
 }
